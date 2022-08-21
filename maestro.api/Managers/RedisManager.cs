9e0b58ef311e419b875bbd773fde6c6373ff27e0
Julien Creach <julien.creach@hotmail.fr>
2022-08-21 16:23:10 +0200

    Add start of redis manager

---
+++ maestro.api/Managers/RedisManager.cs
@@ -0,0 +1,33 @@
+﻿using Maestro.api.Extensions;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System;
+
+namespace Maestro.api.Managers
+{
+    public class RedisManager
+    {
+        private readonly ILogger<RedisManager> logger;
+        private readonly IConnectionMultiplexer redisClient;
+        private readonly IDatabase db;
+        public RedisManager(IConnectionMultiplexer redisClient, ILogger<RedisManager> logger)
+        {
+            this.redisClient = redisClient;
+            db = this.redisClient.GetDatabase();
+            this.logger = logger;
+        }
+
+        public string Ping()
+        {
+            try
+            {
+                return db.Ping().ToString();
+            }
+            catch (Exception ex)
+            {
+                this.logger.Error(ex.Message);
+                return string.Empty;
+            }
+        }
+    }
+}
