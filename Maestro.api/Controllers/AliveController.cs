e220adc65f8d585482f197dd7c8ca11b6b86d113
Julien Creach <julien.creach@hotmail.fr>
2022-08-21 16:41:15 +0200

    Add alive controller

---
+++ Maestro.api/Controllers/AliveController.cs
@@ -0,0 +1,23 @@
+﻿using Maestro.api.Managers;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Maestro.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliveController : ControllerBase
+    {
+        private readonly RedisManager redisManager;
+        public AliveController(RedisManager redisManager)
+        {
+            this.redisManager = redisManager;
+        }
+
+        [HttpGet, ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public string Ping()
+        {
+            return  redisManager.Ping();
+        }
+    }
+}
