95f033f0e01f5c679769143660613dac81d58bec
Julien Creach <julien.creach@hotmail.fr>
2022-08-21 16:41:02 +0200

    Configure redis manager and redis instance for DI

--- Maestro.api/Program.cs
+++ Maestro.api/Program.cs
@@ -1,7 +1,9 @@
+using Maestro.api.Managers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<RedisManager>();
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.Configuration["Redis"]));
 builder.Services.AddLogging(x =>
 {
     x.ClearProviders();
